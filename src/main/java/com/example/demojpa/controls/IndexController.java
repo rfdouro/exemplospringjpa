11da5f00e46af9535657011e79119c76fa4367dd
Rômulo F. Douro <rfdouro@gmail.com>
2024-08-20 16:55:30 -0300

    codigo: PUSH atualização do projeto e readme

--- src/main/java/com/example/demojpa/controls/IndexController.java
+++ src/main/java/com/example/demojpa/controls/IndexController.java
@@ -1,8 +1,10 @@
 package com.example.demojpa.controls;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -13,10 +15,15 @@ import com.example.demojpa.models.AutorRepository;
 import com.example.demojpa.models.Livro;
 import com.example.demojpa.models.LivroRepository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping({ "", "/" })
@@ -27,20 +34,41 @@ public class IndexController {
  @Autowired
  LivroRepository livroRepository;
 
+ // usado para queries customizadas aqui
+ @PersistenceContext
+ private EntityManager entityManager;
+
  @GetMapping("")
  public String index() {
   return "index";
  }
 
+ @GetMapping("/custom")
+ public @ResponseBody List custom() {
+  Query q = entityManager.createQuery("select A.nome, L.titulo from Autor A join A.livros L");
+  return q.getResultList();
+ }
+
  @GetMapping("/autor")
  public String abreListaAutor(Model model) {
-  model.addAttribute("autores", autorRepository.findAll());
+  //retorna a primeira página com 10 elementos no máximo
+  model.addAttribute("autores", autorRepository.listaOrdenadoPorNomeComPaginacao(PageRequest.of(0, 10)));
+  //retorna a lista buscando todos que começam o nome por 'a'
+  // model.addAttribute("autores", autorRepository.listaOrdenadoPorNomeInicio("a%"));
   return "autores";
  }
 
  @PostMapping("/autor/add")
  public String cadastraAutor(String nome) {
   Autor a = new Autor();
+  a.setNome(nome);
+  autorRepository.save(a);
+  return "redirect:/autor";
+ }
+
+ @GetMapping("/autor/upd/{id}")
+ public String atualizaAutor(@PathVariable("id") Long id, String nome) {
+  Autor a = autorRepository.getReferenceById(id);
   a.setNome(nome);
   autorRepository.save(a);
   return "redirect:/autor";
@@ -57,7 +85,7 @@ public class IndexController {
  @GetMapping("/livro")
  public String abreListaLivro(Model model) {
   model.addAttribute("livros", livroRepository.findAll());
-  model.addAttribute("autores", autorRepository.findAll());
+  model.addAttribute("autores", autorRepository.findAllByOrderByNome());
   return "livros";
  }
 
